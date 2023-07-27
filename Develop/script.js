ed6bd18f3c77b4ac37c321377449da4165ff632c
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-07-27 14:40:10 -0500

    removed css outlines

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,7 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// wraps all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+$(document).ready(function () {
 
-
-  // TODO: Add code to display the current date in the header of the page.
+  //add code to display the current date in the header of the page
   var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
@@ -41,12 +39,7 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
     
   };
 
-   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+ 
   var saveButtonEl = $("button");
   saveButtonEl.on("click", function(){
     var text = $(this).siblings("textarea").val();
@@ -61,8 +54,9 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
       $("#" + savedInput).find("textarea").val(output);
     };
 
-    
+
   updatePPF();
+});
  
   
 
